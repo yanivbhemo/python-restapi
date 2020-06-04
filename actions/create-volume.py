f2cf2bca0af84e80117a12c62cf6b19847b2789d
yanivbhemo <yaniv.bhemo@gmail.com>
2020-06-04 11:17:53 +0300

    adaptations to storage automation

---
+++ actions/create-volume.py
@@ -0,0 +1,6 @@
+class CreateVolume:
+    array = ""
+    array_type = ""
+
+    def __init__(self):
+        pass
---
+++ storage-adapters/xio.py
@@ -0,0 +1,18 @@
+class XioHandler:
+    def __init__(self):
+        pass
+
+    def check_suitable_array(self):
+        pass
+
+    def create_volume(self):
+        pass
+
+    def create_cg(self):
+        pass
+
+    def add_vol_to_cg(self):
+        pass
+
+    def take_snapshot_set(self):
+        pass
